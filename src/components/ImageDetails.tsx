90f72557c811628f27c14a4c6251f40ba5887cd4
geeetansh <123809200+geeetansh@users.noreply.github.com>
2025-05-21 23:56:39 +0530

    Remove Prompt Section from ImageDetails Component

--- src/components/ImageDetails.tsx
+++ src/components/ImageDetails.tsx
@@ -135,13 +135,6 @@ export function ImageDetails({ open, onOpenChange, image }: ImageDetailsProps) {
                   </div>
                 </div>
                 
-                <div>
-                  <h3 className="text-lg font-medium mb-2">Prompt</h3>
-                  <div className="bg-muted/20 rounded-lg p-3 border">
-                    <p className="whitespace-pre-line text-sm">{image.prompt}</p>
-                  </div>
-                </div>
-                
                 {/* Edited Images List */}
                 {magicEditEnabled && (
                   <EditedImagesList 
